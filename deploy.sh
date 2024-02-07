ede0844c66c2d292eba359a1d301a9fff15a7388
BlackieOver21 <94404629+BlackieOver21@users.noreply.github.com>
2024-02-07 21:59:35 +0200

    Node packages
    Added node packages needed for server run

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-scp server.js 1bersutskyi@pascal.fis.agh.edu.pl:Desktop/prj1
-
-scp connection/* 1bersutskyi@pascal.fis.agh.edu.pl:Desktop/prj1/connection
-
-scp -r Strona/* 1bersutskyi@pascal.fis.agh.edu.pl:public_html/prj
+scp -r ./* 1bersutskyi@pascal.fis.agh.edu.pl:Desktop/prj_TI
